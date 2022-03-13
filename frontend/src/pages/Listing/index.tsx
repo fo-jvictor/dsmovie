e86e245a0323faf17e517604bd0b338ca8650860
joao victor <jvictor-01@hotmail.com>
2022-03-13 14:23:47 -0300

    comentario movie

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -33,13 +33,13 @@ function Listing() {
         });
     }, [pageNumber]);
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+    //const movie = {
+      //  id: 1,
+        //image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        //title: "The Witcher",
+        //count: 2,
+        //score: 4.5
+   // };
 
     const handlePageChange = (newPageNumber:number) =>{
         setPageNumber(newPageNumber);
